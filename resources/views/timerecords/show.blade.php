f1a4f0e07b932901a91b68de077a6ef8adc99092
Md. Naim Chowdhury <naimmcduck@gmail.com>
2023-11-12 01:50:22 +0100

    Add recorded by field in show template

--- resources/views/timerecords/show.blade.php
+++ resources/views/timerecords/show.blade.php
@@ -4,11 +4,12 @@
     <div class="row justify-content-center">
         <div class="col-sm-4 mt-2">
             <div class="card p-4 mt-5">
-                <p> Entry Number : <b>{{$time_record->id}}</b></p>
                 <p> Project Name: <b>{{$time_record->project->name}}</b></p>
                 <p> Start Time: <b>{{$time_record->start_time}}</b></p>
                 <p> End Time: <b>{{$time_record->end_time}}</b></p>
                 <p> Work Hour: <b>{{$time_record->total_hours}}</b></p>
+                <p> Record Number : <b>{{$time_record->id}}</b></p>
+                <p> Recorded by : <b>{{$time_record->user->name}}</b></p>
                 <a href="/time_records" class="btn btn-dark">Back</a>
         </div>
     </div>
